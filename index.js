fddd2daca4151443511503e0613786e230828ce9
ozodbek-2006 <crazzygpt@gmail.com>
2024-06-11 14:22:36 +0500

    4oy6darss

---
+++ index.js
@@ -0,0 +1,204 @@
+// // Masala 1
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// // Masala 2
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// // Masala 3
+// function square(a) {
+//     return a * a;
+// }
+
+// // Masala 4
+// function cube(a) {
+//     return a * a * a;
+// }
+
+// // Masala 5
+// function average(a, b, c) {
+//     return (a + b + c) / 3;
+// }
+
+// // Masala 6
+// function isEven(a) {
+//     return a % 2 === 0;
+// }
+
+// // Masala 7
+// function isOdd(a) {
+//     return a % 2 !== 0;
+// }
+
+// // Masala 8
+// function max(a, b) {
+//     return a > b ? a : b;
+// }
+
+// // Masala 9
+// function min(a, b) {
+//     return a < b ? a : b;
+// }
+
+// // Masala 10
+// function sumToN(n) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// // Masala 11
+// function factorial(n) {
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// // Masala 12
+// function checkAge(age) {
+//     return age >= 18 ? "Katta" : "Kichik";
+// }
+
+// // Masala 13
+// function isDivisible(a, b) {
+//     return a % b === 0;
+// }
+
+// // Masala 14
+// function isPositive(a) {
+//     return a > 0;
+// }
+
+// // Masala 15
+// function isNegative(a) {
+//     return a < 0;
+// }
+
+// // Masala 16
+// function squareRoot(a) {
+//     return Math.sqrt(a);
+// }
+
+// // Masala 17
+// function difference(a, b) {
+//     return a - b;
+// }
+
+// // Masala 18
+// function maxOfThree(a, b, c) {
+//     return Math.max(a, b, c);
+// }
+
+// // Masala 19
+// function minOfThree(a, b, c) {
+//     return Math.min(a, b, c);
+// }
+
+// // Masala 20
+// function compareToTen(a) {
+//     return a > 10 ? "Katta" : "Kichik";
+// }
+
+// // Masala 21
+// function isSumEven(a, b) {
+//     return (a + b) % 2 === 0;
+// }
+
+// // Masala 22
+// function isDivisibleBy5(a) {
+//     return a % 5 === 0;
+// }
+
+// // Masala 23
+// function isGreater(a, b) {
+//     return a > b;
+// }
+
+// // Masala 24
+// function isDivisibleBy7(a) {
+//     return a % 7 === 0;
+// }
+
+// // Masala 25
+// function powerOf10(a) {
+//     return Math.pow(a, 10);
+// }
+
+// // Masala 26
+// function averageOfTwo(a, b) {
+//     return (a + b) / 2;
+// }
+
+// // Masala 27
+// function lessThan100(a) {
+//     return a < 100 ? "Kichik" : "Katta";
+// }
+
+// // Masala 28
+// function isDivisibleBy2(a) {
+//     return a % 2 === 0;
+// }
+
+// // Masala 29
+// function sqrtOfProduct(a, b) {
+//     return Math.sqrt(a * b);
+// }
+
+// // Masala 30
+// function isSumEqual(a, b, c) {
+//     return a + b === c;
+// }
+
+// // For yordamida ishlanadigan masalalar
+
+// // Masala 1
+// function sumFrom1To10() {
+//     let sum = 0;
+//     for (let i = 1; i <= 10; i++) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// // Masala 2
+// function productFrom1To5() {
+//     let product = 1;
+//     for (let i = 1; i <= 5; i++) {
+//         product *= i;
+//     }
+//     return product;
+// }
+
+// // Masala 3
+// function factorialOfNumber(n) {
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// // Masala 4
+// function printEvenNumbersToN(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 === 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// // Masala 5
+// function printOddNumbersToN(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 !== 0) {
+//             console.log(i);
+//         }
+//     }
+// }
